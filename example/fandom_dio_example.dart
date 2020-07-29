c0a6a8b04bc9784391c4f77d7f8775819a7e568f
Mattias <mattias.erik.karlsson@gmail.com>
2020-07-29 21:43:01 +0200

    Updated examples

--- example/fandom_dio_example.dart
+++ example/fandom_dio_example.dart
@@ -4,6 +4,6 @@ import 'package:fandom/src/fandom.dart';
 Future<void> main() async {
   var fandom = Fandom('https://marvel.fandom.com',
       (path) => Dio().get(path).then((value) => value.data));
-  var result = await fandom.activity.latestActivity();
-  print('result: ${result}');
+  var result = await fandom.search.list('Wolverine');
+  print('result: ${result.items.first.snippet}');
 }
--- example/fandom_http_example.dart
+++ example/fandom_http_example.dart
@@ -6,6 +6,6 @@ import 'package:http/http.dart' as http;
 Future<void> main() async {
   var fandom = Fandom('https://marvel.fandom.com',
       (path) => http.get(path).then((value) => json.decode(value.body)));
-  var result = await fandom.activity.latestActivity();
-  print('result: ${result}');
+  var result = await fandom.search.list('Wolverine');
+  print('result: ${result.items.first.snippet}');
 }
