1e0f01524fb2efac380337126549906500b84189
Mattias <mattias.erik.karlsson@gmail.com>
2020-07-29 21:04:17 +0200

    Added LICENSE information to fandom.dart

--- lib/fandom.dart
+++ lib/fandom.dart
@@ -1,7 +1,8 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+// Copyright (c) 2020, Mattias Karlsson. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
+/// Fandom API library, which makes it easy to access data from specified fandom URL
 library fandom;
 
-export 'src/client.dart';
-export 'src/json_provider.dart';
+export 'src/fandom.dart';
