9cd08e21825d79fb3b287b63d0df5bba082b8be8
Mattias <mattias.erik.karlsson@gmail.com>
2020-07-29 21:02:26 +0200

    Added LICENSE information

--- lib/src/models/section.dart
+++ lib/src/models/section.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, Mattias Karlsson. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
 import 'package:json_annotation/json_annotation.dart';
 
 import 'section_content.dart';
