82da279a66cbc8c32afea774e3550127fac4bcf0
Mattias <mattias.erik.karlsson@gmail.com>
2020-07-30 10:30:21 +0200

    Added resources for articles, wam, and wikis

--- lib/src/fandom.dart
+++ lib/src/fandom.dart
@@ -20,18 +20,37 @@ class Fandom {
   RelatedPagesResource _relatedPages;
   SearchResource _search;
   UserResource _user;
+  ArticlesResource _articles;
+  WamResource _wam;
+  WikisResource _wikis;
 
+  /// Get information about the latest user activity on the current wiki
   ActivityResource get activity => _activity;
 
+  /// Get simplified article contents
+  ArticlesResource get articles => _articles;
+
+  /// get wiki data, including key values, navigation data, and more
   MercuryResource get mercury => _mercury;
 
+  /// Get wiki navigation links (the main menu of given wiki)
   NavigationResource get navigation => _navigation;
 
+  /// Get pages related to a given article ID
   RelatedPagesResource get relatedPages => _relatedPages;
 
+  /// Get results for combined (wiki and cross-wiki) search
+  /// Find suggested phrases for chosen query
   SearchResource get search => _search;
 
+  /// Get details about selected users
   UserResource get user => _user;
+
+  /// Get WAM score starting and last available dates
+  WamResource get wam => _wam;
+
+  /// Get wikis which name or topic match a keyword
+  WikisResource get wikis => _wikis;
 
   Fandom._internal(this.baseUrl, this.jsonProvider);
 
